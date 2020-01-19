302d3b64ddb587693ba3dba6ac618eb82e0ee16c
KAMAL <KAMAL@192.168.0.6>
2020-01-19 19:54:59 +0530

    new code

--- demo/src/test/java/demo/DemoJenkins.java
+++ demo/src/test/java/demo/DemoJenkins.java
@@ -1,12 +1,22 @@
 package demo;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class DemoJenkins {
 	
-	@Test
-	public void demo() {
-		System.out.println("rajkamal");
-	}
-
+	public class Facebooklogin {
+		WebDriver driver;
+		@Test
+		public void login() throws Exception {
+			System.setProperty("webdriver.chrome.driver", "I:\\chromedriver.exe");
+			driver=new ChromeDriver();
+			driver.get("https://www.facebook.com");
+			Thread.sleep(5000);
+			driver.findElement(By.id("email")).sendKeys("rajkamalmmt@gmail.com");
+			driver.findElement(By.id("pass")).sendKeys("muleesb");
+			driver.findElement(By.xpath("//*[@id=\"u_0_b\"]")).click();
+			
 }
